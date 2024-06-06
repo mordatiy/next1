9037d47c79a148f69d27f4d3b38fff9ca4c6dfdf
mordatiy <mordatiykd@gmail.com>
2024-06-06 21:59:47 +0300

    metadata

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,10 @@
+import type {Metadata} from "next";
 
-
+export const metadata: Metadata = {
+    title: "About",
+    description: "About 2024",
+    keywords: "about",
+};
 export default function About() {
     return (
         <div>
--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -1,6 +1,24 @@
 import React from "react";
 import Link from "next/link";
 import Post from "@/app/components/Post";
+import { Metadata, ResolvingMetadata } from 'next'
+
+type TypeProps = {
+    params: { id: string }
+    searchParams: { [key: string]: string | string[] | undefined }
+}
+
+export async function generateMetadata(
+    { params, searchParams }: TypeProps,
+    parent: ResolvingMetadata
+): Promise<Metadata> {
+    console.log(params)
+    const post = await fetchData(params.id)
+    return {
+        title: `Post: ${post.title}`,
+        description: post.body,
+    }
+}
 
 async function fetchData(id: string): Promise<string> {
     const result = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
